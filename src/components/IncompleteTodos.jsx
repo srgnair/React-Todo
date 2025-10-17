b0bf179052883f3cf854d721d571b2b8cf0bc2b4
srgnair <air.shirogane@gmail.com>
2025-10-17 16:26:05 +0900

    refactor: 編集フォームコンポーネントを追加

--- src/components/IncompleteTodos.jsx
+++ src/components/IncompleteTodos.jsx
@@ -1,3 +1,5 @@
+import { EditingForm } from "./EditingForm";
+
 export const IncompleteTodos = (props) => {
   const { 
     todos, 
@@ -8,46 +10,40 @@ export const IncompleteTodos = (props) => {
     onStartEdit,
     onChangeEditingText,
     onSaveEdit,
-    onCancelEdit, } = props;
-    return (
-      <div className="incomplete-area">
-        <p className="title">未完了のTODO</p>
-        <ul>
-          {todos.map((todo, index) => {
-            const isEditing =
-              editing && editing.list === "incomplete" && editing.index === index;
-  
-            return (
-              <li key={index}>
-                  {isEditing ? (
-                    <div className="list-low">
-                      <input
-                        value={editingText}
-                        onChange={onChangeEditingText}
-                        onKeyDown={(e) => {
-                          if (e.key === "Enter") onSaveEdit();
-                          if (e.key === "Escape") onCancelEdit();
-                        }}
-                        autoFocus
-                      />
-                      <button onClick={onSaveEdit}>保存</button>
-                      <button onClick={onCancelEdit}>キャンセル</button>
-                    </div>
-                  ) : (
-                    <div className="list-low">
-                      <p className="todo-item">{todo}</p>
-                      <button onClick={() => onStartEdit("incomplete", index)}>
-                        編集
-                      </button>
-                      <button onClick={() => onClickComplete(index)}>完了</button>
-                      <button onClick={() => onClickDelete(index)}>削除</button>
-                    </div>
-                  )}
+    onCancelEdit, 
+  } = props;
 
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
-  };
+  return (
+    <div className="incomplete-area">
+      <p className="title">未完了のTODO</p>
+      <ul>
+        {todos.map((todo, index) => {
+          const isEditing =
+            editing && editing.list === "incomplete" && editing.index === index;
+
+          return (
+            <li key={index}>
+              {isEditing ? (
+                <EditingForm
+                  editingText={editingText}
+                  onChange={onChangeEditingText}
+                  onSave={onSaveEdit}
+                  onCancel={onCancelEdit}
+                />
+              ) : (
+                <div className="list-low">
+                  <p className="todo-item">{todo}</p>
+                  <button onClick={() => onStartEdit("incomplete", index)}>
+                    編集
+                  </button>
+                  <button onClick={() => onClickComplete(index)}>完了</button>
+                  <button onClick={() => onClickDelete(index)}>削除</button>
+                </div>
+              )}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
